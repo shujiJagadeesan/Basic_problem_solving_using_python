747f025783b52365365064e49a17b6a684d9f992
shujiJagadeesan <shujijagadeesan@gmail.com>
2025-01-17 19:28:49 +0530

    armstrong number concept done

--- armstrong_number.py
+++ armstrong_number.py
@@ -1,18 +1,17 @@
 def is_armstrong_number(given_number):
     number = given_number
-    digit_count = 0
+    digit_count = len(str(number))
     sum=0
-    while number > 0:
-        digit_count += 1
-        number //= 10
-    print(digit_count)
+    # while number > 0:
+    #     digit_count += 1
+    #     number //= 10
+    # print(digit_count)
 
-    while given_number > 0:
-        # digit_count += 1
-        digit = given_number % 10
+    while number > 0:
+        digit = number % 10
         answer = digit ** digit_count
         sum += answer
-        given_number //= 10
+        number //= 10
     return sum
 given_number = 1634
 armstrong_number = is_armstrong_number(given_number)
