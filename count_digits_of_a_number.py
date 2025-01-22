54d9a89966d177a317090503b9636092a42a42ed
shujiJagadeesan <shujijagadeesan@gmail.com>
2025-01-22 20:33:49 +0530

    counting the number of digits in a number

---
+++ count_digits_of_a_number.py
@@ -0,0 +1,6 @@
+def count_digits_of_a_number(number):
+    digit_count = len(str(number))
+    return digit_count
+number = 1234567890
+count_of_digits_of_the_number = count_digits_of_a_number(number)
+print(count_of_digits_of_the_number)
